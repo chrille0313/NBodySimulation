0ee1f9f6252b0aa25055f7659be2111baaba1c8a
Christian <chrille_0313@hotmail.com>
2022-04-20 00:22:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ from Engine.engine import *
 from body import Body
 from quadtree import QuadTree
 
-import pygame
+import pygame  # Only for keycodes!
 from numpy import random
 
 
 class Game(PygameApp):
-	"""
-	TODO:
-		- Make examples
-	"""
-
 	def __init__(self, windowSize: tuple):
 		super().__init__(windowSize, 60, camera=Camera2D(Vector2(0, 0), Vector2(windowSize), 1))
 
@@ -20,49 +15,61 @@ class Game(PygameApp):
 		boundScale = 100
 		self.bounds = Rect(-self.windowSize.x / 2 * boundScale, self.windowSize.y / 2 * boundScale, self.windowSize.x * boundScale, self.windowSize.y * boundScale)
 
+		# Generate random bodies
 		bodies = 300
 		maxSpeed = 40
 		minMass, maxMass = 10, 1000
-		# self.bodies = [Body(Vector2(1, 250), 500, Vector2(0, -3)), Body(Vector2(2, -250), 250, Vector2(0, 3))]
-		self.bodies = [Body(Vector2(random.randint(self.bounds.x / scale, (self.bounds.x + self.bounds.width) / scale),
-		                            random.randint((self.bounds.y - self.bounds.height) / scale, self.bounds.y / scale)),
-		               random.randint(minMass, maxMass),
-		               Vector2((random.rand() * 2 - 1), (random.rand() * 2 - 1)) * maxSpeed)
-		               for _ in range(bodies)]
 
+		self.bodies = []  # Large center body
+
+		for _ in range(bodies):
+			pos = Vector2(random.randint(self.bounds.x / scale, (self.bounds.x + self.bounds.width) / scale),
+			              random.randint((self.bounds.y - self.bounds.height) / scale, self.bounds.y) / scale)
+
+			mass = random.randint(minMass, maxMass)
+
+			self.bodies.append(Body(pos, mass))
+
+		# Give bodies initial velocity to spin around center
 		for body in self.bodies:
 			toCenter = -body.position.normalize()
 			body.velocity = Vector2(toCenter.y, -toCenter.x) * maxSpeed
 
-		self.bodies.append(Body(Vector2(0, 0), 1000000, Vector2(0, 0)))
+		self.bodies.append(Body(Vector2(0, 0), 1000000, Vector2(0, 0)))  # Large center body
 
 	def draw_debug(self):
 		self.quadTree.draw(self)
 
 	def camera_control(self):
 		if self.isKeyPressed[pygame.K_w]:
-			self.mainCamera.position += Vector2(0, 1) * self.mainCamera.moveSpeed / self.mainCamera.zoom
+			self.mainCamera.move(Vector2(0, 1))
 		if self.isKeyPressed[pygame.K_s]:
-			self.mainCamera.position += Vector2(0, -1) * self.mainCamera.moveSpeed / self.mainCamera.zoom
+			self.mainCamera.move(Vector2(0, -1))
 		if self.isKeyPressed[pygame.K_a]:
-			self.mainCamera.position += Vector2(-1, 0) * self.mainCamera.moveSpeed / self.mainCamera.zoom
+			self.mainCamera.move(Vector2(-1, 0))
 		if self.isKeyPressed[pygame.K_d]:
-			self.mainCamera.position += Vector2(1, 0) * self.mainCamera.moveSpeed / self.mainCamera.zoom
+			self.mainCamera.move(Vector2(1, 0))
 
 		if self.isKeyPressed[pygame.K_q]:
-			self.mainCamera.zoom *= self.mainCamera.zoomSpeed
+			self.mainCamera.zoom_in()
 		if self.isKeyPressed[pygame.K_e]:
-			self.mainCamera.zoom /= self.mainCamera.zoomSpeed
+			self.mainCamera.zoom_out()
 
 	def on_draw(self):
 		for body in self.bodies:
 			body.draw(self)
 
-		# self.draw_debug()
+		if self.debug:
+			self.draw_debug()
 
 	def on_update(self):
 		if self.isKeyPressed[pygame.K_ESCAPE]:
 			self.quit()
+
+		if self.isKeyPressed[pygame.K_SPACE]:
+			self.debug = True
+		else:
+			self.debug = False
 
 		self.camera_control()
 
@@ -81,7 +88,7 @@ class Game(PygameApp):
 			"""
 
 			# QUADTREE GRAVITY
-			self.quadTree.gravity(body, 1)
+			self.quadTree.gravity(body, 1, 5)
 
 		# COLLISION DETECTION
 		for i, body in enumerate(self.bodies):
